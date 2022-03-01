0ede4275a38962770bc7cb1d18a328ff79dc63ba
nikhileshmeher24 <93664022+nikhileshmeher24@users.noreply.github.com>
2022-03-02 01:04:16 +0530

    Create weather_forecast_emailer.py

---
+++ weather_forecast_emailer.py
@@ -0,0 +1,43 @@
+import smtplib,requests
+from bs4 import BeautifulSoup
+import smtplib,ssl,getpass
+import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+content=''
+def extract_weather(url):
+    print("Extracting weather\n")
+    info=''
+    responce=requests.get(url)
+    soup=BeautifulSoup(responce.content,'html.parser')
+    temp=soup.find("div", attrs={"class": "h2"}).text
+    sky=soup.find("p").text
+    info="City: "+city+" \nTemperature: "+temp+"\nSky: "+sky+"\n"
+    return info
+
+now=datetime.datetime.now()
+city=input("Enter city")
+url="https://www.timeanddate.com/weather/india/"+city
+cnt=extract_weather(url)
+content+=cnt
+content+=('<br>-------<br>')
+content+=('End of message')
+print(content)
+smtp_server="smtp.gmail.com"
+port=587
+sender_email=""
+receiver_email=""
+message=MIMEMultipart()
+message["Subject"]="Weather today! "+"Dated: "+str(now.day)+"/"+str(now.month)+"/"+str(now.year)
+message["From"]=sender_email
+message["To"]=receiver_email
+message.attach(MIMEText(content,'html'))
+password=""
+context=ssl.create_default_context()
+server=smtplib.SMTP(smtp_server,port)
+server.starttls(context=context)
+server.login(sender_email,password)
+print("successful\n")
+
+
+server.sendmail(sender_email,receiver_email,message.as_string())
