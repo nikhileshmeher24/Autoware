422e13bbf5b0935d132b2ffe3478e8a5e7548eb0
nikhileshmeher24 <93664022+nikhileshmeher24@users.noreply.github.com>
2022-03-03 14:09:51 +0530

    Update weather_forecast_emailer.py

--- weather_forecast_emailer.py
+++ weather_forecast_emailer.py
@@ -1,33 +1,33 @@
 import smtplib,ssl,getpass,requests,datetime           #smtplib defines an SMTP, ssl for secure connection
 from bs4 import BeautifulSoup
-from email.mime.multipart import MIMEMultipart
+from email.mime.multipart import MIMEMultipart         #for sending emails
 from email.mime.text import MIMEText
 
 
-
-now=datetime.datetime.now()
-content=''
 def extract_weather(url):
     print("Extracting weather\n")
-    info=''
     responce=requests.get(url)                           #get the whole html code
     soup=BeautifulSoup(responce.content,'html.parser')   #organize the html code
     temp=soup.find("div", attrs={"class": "h2"}).text    #extract temperature from the html code
     sky=soup.find("p").text                              #extract sky situation
-    info="City: "+city+" | Temperature: "+temp+" | Sky: "+sky+"\n"
-    return info
+    data="City: "+city+" | Temperature: "+temp+" | Sky: "+sky+"\n"
+    return data
 
-sender_email=""
-receiver_email=""
+
+sender_email="nikhileshmeher2021@gmail.com"
+receiver_email="nikhileshmeher24@gmail.com"
 password=getpass.getpass(prompt="Enter your password: ")
-city=input("Enter city")
+city=input("Enter city: ")
 url="https://www.timeanddate.com/weather/india/"+city
 content=extract_weather(url)
+content+=('<br>-----<br>')
+content+=('Have a good day!')
 
 
 
 
 message=MIMEMultipart()     #MIMEMultipart is used when we have attachments or want to provide alternative versions of the same content (e.g. a plain text/HTML version)
+now=datetime.datetime.now()
 message["Subject"]="Weather today! "+"Dated: "+str(now.day)+"/"+str(now.month)+"/"+str(now.year)
 message["From"]=sender_email
 message["To"]=receiver_email
@@ -35,13 +35,11 @@ message.attach(MIMEText(content,'html'))
 
 
 
-context=ssl.create_default_context()   #A necessary security measure- Ensures secure default settings
+context=ssl.create_default_context()   #A security measure- Ensures secure default settings
 smtp_server="smtp.gmail.com"           #for establishing a secure connection to the GMAIL server
 port=587
 server=smtplib.SMTP(smtp_server,port)
 server.starttls(context=context)
 server.login(sender_email,password)
-print("successful\n")
-
-
 server.sendmail(sender_email,receiver_email,message.as_string())
+print("successful\n")
